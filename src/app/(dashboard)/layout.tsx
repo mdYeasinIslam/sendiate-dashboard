fd6742ca1e2cb0cef1910c5be80d5a38b9f781b9
fiamanillah <fiamanillahb2@gmail.com>
2025-05-15 03:05:36 +0600

    Layout fix

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import '../globals.css';
+import MainLayout from '@/components/MainLayout';
 const geistSans = Geist({
     variable: '--font-geist-sans',
     subsets: ['latin'],
@@ -24,7 +25,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                {children}
+                <MainLayout>{children}</MainLayout>
             </body>
         </html>
     );
