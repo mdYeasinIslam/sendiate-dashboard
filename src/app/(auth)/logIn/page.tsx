3fdd5cef9861bc832877f9d85f8399c937dacd90
mdYeasinIslam <hyeasinislam@gmail.com>
2025-05-20 05:49:40 +0600

    some error fixing

--- src/app/(auth)/logIn/page.tsx
+++ src/app/(auth)/logIn/page.tsx
@@ -15,7 +15,7 @@ const LoginPage = () => {
         const formData = new FormData(e.currentTarget);
         const email = formData.get("email");
         const password = formData.get("password");
-        // console.log( email, password);
+        console.log( email, password);
        
     };
 
