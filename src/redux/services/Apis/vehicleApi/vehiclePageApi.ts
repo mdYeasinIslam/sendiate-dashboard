804d6d068dad7e8cedb4aea75a21ee6d78998080
mdYeasinIslam <hyeasinislam@gmail.com>
2025-05-31 23:07:51 +0600

    try to fix cors related issue

--- src/redux/services/Apis/vehicleApi/vehiclePageApi.ts
+++ src/redux/services/Apis/vehicleApi/vehiclePageApi.ts
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
  export const vehiclePageApi = createApi({
      reducerPath: 'vehiclePageApi',
      baseQuery: fetchBaseQuery({
-         // baseUrl: 'https://patrkamh.onrender.com/api/v1',
-         baseUrl: 'http://10.0.30.91:5001/api/v1',
+         baseUrl: 'https://patrkamh.onrender.com/api/v1',
+        //  baseUrl: 'http://10.0.30.91:5001/api/v1',
          prepareHeaders: (headers) => {
              if (typeof window !== 'undefined') {
                  const rawToken = localStorage.getItem('token');
